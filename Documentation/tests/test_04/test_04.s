415d254565c3f24a2e4b36aecbbb96545f379930
Chris-tiee <czb00@mail.aub.edu>
2023-05-24 09:31:02 +0300

    modified:   Documentation/Project Documentation.pdf 	renamed:    Documentation/Project Report - Part 2.pdf -> Documentation/Testing.pdf 	new file:   Documentation/tests/test_01/rom_test_01.vhd 	new file:   Documentation/tests/test_01/test_01.s 	new file:   Documentation/tests/test_01/test_01.txt 	new file:   Documentation/tests/test_02/rom_test_02.vhd 	new file:   Documentation/tests/test_02/test_02.s 	new file:   Documentation/tests/test_02/test_02.txt 	new file:   Documentation/tests/test_03/rom_test_03.vhd 	new file:   Documentation/tests/test_03/test_03.s 	new file:   Documentation/tests/test_03/test_03.txt 	new file:   Documentation/tests/test_04/rom_test_04.vhd 	new file:   Documentation/tests/test_04/test_04.s 	new file:   Documentation/tests/test_04/test_04.txt 	new file:   Documentation/tests/test_05/rom_test_05.txt 	new file:   Documentation/tests/test_05/test_05.s 	new file:   Documentation/tests/test_05/test_05.txt 	new file:   Documentation/tests/test_06/rom_test_06.txt 	new file:   Documentation/tests/test_06/test_06.s 	new file:   Documentation/tests/test_06/test_06.txt 	new file:   Documentation/tests/test_07/rom_test_07.txt 	new file:   Documentation/tests/test_07/test_07.s 	new file:   Documentation/tests/test_07/test_07.txt 	new file:   Documentation/tests/test_08/rom_test_08.txt 	new file:   Documentation/tests/test_08/test_08.s 	new file:   Documentation/tests/test_08/test_08.txt

---
+++ Documentation/tests/test_04/test_04.s
@@ -0,0 +1,9 @@
+addi x5 x5 100
+addi x6 x4 5
+addi x10 x0 9
+nop
+mul x8 x6 x5
+mul x9 x5 x10
+addi x7 x4 100
+addi x6 x6 11
+addi x9 x6 10
---
+++ Documentation/tests/test_08/test_08.s
@@ -0,0 +1,5 @@
+addi x5 x5 100
+loop: 
+addi x6 x6 10
+bne x5 x6 loop
+addi x7 x7 10
